411ea2171499c9dfd094ffd005a7fd5f054f0c7a
YaMing <dnawym@163.com>
2014-10-15 11:11:38 -0400

    finished bull's eye

--- BullesEye/BullesEye/ViewController.swift
+++ BullesEye/BullesEye/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
 
@@ -142,6 +143,13 @@ class ViewController: UIViewController {
     @IBAction func startOver() {
         startNewGame()
         updateLabels()
+        
+        let transition = CATransition()
+        transition.type = kCATransitionFade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        
+        view.layer.addAnimation(transition, forKey: nil)
     }
 }
 
