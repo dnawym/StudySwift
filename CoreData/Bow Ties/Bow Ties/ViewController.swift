e545c5fb6d89c586679188f389eb65026bf411b1
dnawym <dnawym@163.com>
2014-12-28 21:29:55 +0800

    CoreData 2 - Bow Ties

---
+++ CoreData/Bow Ties/Bow Ties/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Bow Ties
+//
+//  Created by Pietro Rea on 6/25/14.
+//  Copyright (c) 2014 Razeware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var segmentedControl: UISegmentedControl!
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var ratingLabel: UILabel!
+  @IBOutlet weak var timesWornLabel: UILabel!
+  @IBOutlet weak var lastWornLabel: UILabel!
+  @IBOutlet weak var favoriteLabel: UILabel!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    
+  }
+  
+  @IBAction func segmentedControl(control: UISegmentedControl) {
+    
+  }
+
+  @IBAction func wear(sender: AnyObject) {
+    
+  }
+  
+  @IBAction func rate(sender: AnyObject) {
+    
+  }
+}
+
